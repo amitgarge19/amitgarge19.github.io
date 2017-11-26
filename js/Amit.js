87dacabb69a5377aed3fea9d06dea7d6a48671d2
Amit Garge <ameet.garge@gmail.com>
2017-11-26 14:31:21 +0530

    Final Portfolio

---
+++ js/Amit.js
@@ -0,0 +1,7 @@
+function formSubmission()
+{
+	//Currently, the code of this form is under construction, after clicking the 'Submit' button, 
+	//it will displays the same
+
+	alert("Code under construction, Sorry for the inconvenience!");	
+}

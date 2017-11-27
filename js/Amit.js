ded492e2a3cf4445dee4e057ce9105c2e1aa5d44
amitgarge19 <31763510+amitgarge19@users.noreply.github.com>
2017-11-27 12:03:39 +0530

    Delete Amit.js

--- js/Amit.js
+++
@@ -1,7 +0,0 @@
-function formSubmission()
-{
-	//Currently, the code of this form is under construction, after clicking the 'Submit' button, 
-	//it will displays the same
-
-	alert("Code under construction, Sorry for the inconvenience!");	
-}
